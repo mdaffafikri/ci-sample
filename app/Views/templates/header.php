037bde92a5ae04311b695fbed5bb879b512950ed
daps <daffafikri@ymail.com>
2020-10-05 06:48:26 +0700

    add navbar in header template

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -10,3 +10,9 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Home</a>
+        </div>
+    </nav>

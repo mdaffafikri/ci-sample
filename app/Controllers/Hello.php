5b2f671b6683c2c626d879e5618922575a7aa658
daps <daffafikri@ymail.com>
2020-10-05 06:45:34 +0700

    modify Hello controller & adding templates

--- app/Controllers/Hello.php
+++ app/Controllers/Hello.php
@@ -6,8 +6,17 @@ class Hello extends BaseController
 	{
         // $greetings = "Welcome";
 
-        $arr['greetings'] = "hello world";
+		$arr['greetings'] = "hello world";
+		$arr['title'] = "This is Index";
+				
         return view('hello', $arr);        
+	}
+
+	public function showname($name = "bro")
+	{
+		echo view('templates/header');
+		echo $name;
+		echo view('templates/footer');
 	}
 
 	//--------------------------------------------------------------------

2a141635375ad3b6ca6851341b21d215ee974f0e
daps <daffafikri@ymail.com>
2020-10-05 19:12:51 +0700

    dahlah

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -14,12 +14,24 @@ class Pages extends BaseController
 
     public function about()
     {
-        $data['title'] = "About";
-        $data['test'] = [1,2,3,4];
+        // $data['title'] = "About";
+        $data = [
+            'title' => 'About',
+            'biografi' => [
+                [
+                'name' => 'John Doe',
+                'alamat' => 'jl. lorem ipsum'
+                ],
+                [
+                'name' => 'Harry Potter',    
+                'alamat' => 'Hogwards cabang wales'
+                ]
+            ]
+        ];
 
-        echo view('templates/header', $data);
-        echo view('pages/about');
-        echo view('templates/footer');
+        // echo view('templates/header', $data);
+        return view('pages/about', $data);
+        // echo view('templates/footer');
     }
 
     //--------------------------------------------------------------------
--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,5 +1,16 @@
+<?= $this->extend('templates/layout'); ?>
+
+<?= $this->section('content'); ?>
+
 <div class="container mt-2">
     <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum blanditiis consectetur voluptatem quos adipisci repellat aliquid, porro ullam recusandae mollitia velit ducimus, excepturi aut error iusto? Sequi accusamus velit unde!</p>    
-    <?= dd($test) ?>    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum blanditiis consectetur voluptatem quos adipisci repellat aliquid, porro ullam recusandae mollitia velit ducimus, excepturi aut error iusto? Sequi accusamus velit unde!</p>        
+    <?php foreach($biografi as $list) : ?>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"> <?= $list['name'] ?></li>
+            <li class="breadcrumb-item"> <?= $list['alamat'] ?></li>            
+        </ol>
+    <?php endforeach; ?>
 </div>
+
+<?= $this->endSection(); ?>
